19bcf730236bc772cab7047a486d98afccc2df81
Junyi Zhou <junyi.zhou@emory.edu>
2024-02-29 14:54:52 -0500

    Added the login features,expected to have logout, let me add Ryan to the Autho0 team

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React, { useState  } from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import ChatbotUi from './components/ChatbotUi';
 import DataCollectionPage from './components/DataCollectionPage';
+import { useAuth0 } from "@auth0/auth0-react";
 
-function SideButton({ value , onClick}) {
+
+
+// Define a LoginButton component that calls the loginWithRedirect method when clicked
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+//Define a LogoutButton component that calls the logout method when clicked
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  );
+};
+
+function SideButton({ value, onClick }) {
   return (
     <button className='sideButton' onClick={onClick}>
       {value}
@@ -11,17 +32,23 @@ function SideButton({ value , onClick}) {
   );
 }
 
+
+
+
 const App = () => {
   const [activeInterface, setActiveInterface] = useState('ChatbotUi');
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Pideatric Airway Management Assistant</h2>
+        <h2>Pediatric Airway Management Assistant</h2>
+        <div style={{ marginLeft: 'auto' }}>
+          <LoginButton />
+        </div>
       </header>
       <div className="app-body">
         <div className="sidebar">
-          <SideButton value={'Airway Management Assistant'} onClick={() => setActiveInterface('ChatbotUi')}/>
-          <SideButton value={'Data Collection Assistant'} onClick={() => setActiveInterface('DataCollectUi')}/>
+          <SideButton value={'Airway Management Assistant'} onClick={() => setActiveInterface('ChatbotUi')} />
+          <SideButton value={'Data Collection Assistant'} onClick={() => setActiveInterface('DataCollectUi')} />
         </div>
         <div className="chat-container">
           {activeInterface === 'ChatbotUi' ? <ChatbotUi /> : <DataCollectionPage />}
