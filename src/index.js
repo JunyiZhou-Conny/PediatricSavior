fb0139db7673ec02c02cd7bc1e934dff80c367a5
Junyi Zhou <junyi.zhou@emory.edu>
2024-03-03 17:38:19 -0500

    delete all unecessary files assciated react create

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react'; // Import Auth0Provider
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
