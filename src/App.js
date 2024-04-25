8433cd3792b9eab096b1cc4534d72f7344bb3786
Ryan <ryan.meng@emory.edu>
2024-04-25 17:37:14 -0400

    landing logout v1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,53 @@ import React, { useEffect, useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import AuthenticatedApp from './components/AuthenticatedApp';
 import UnauthApp from './components/UnauthApp';
-import NavigationBar from './components/NavigationBar/NavigationBar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Puff } from 'react-loader-spinner';
 import './styles.css';
 
 const App = () => {
   const [activeInterface, setActiveInterface] = useState(null);
-  const { isAuthenticated, user, isLoading, loginWithRedirect } = useAuth0();
+  const { isAuthenticated, user, isLoading, loginWithRedirect, logout } = useAuth0();
   const [isUserAdmin, setIsUserAdmin] = useState(false);
   const [isProfileModalVisible, setIsProfileModalVisible] = useState(false);
   const [chatbotLoaded, setChatbotLoaded] = useState(false);
   const [showParticipantIDPopup, setShowParticipantIDPopup] = useState(false);
-  const [participantID, setParticipantID] = useState(''); // New state for participantID
+  const [participantID, setParticipantID] = useState('');
 
   useEffect(() => {
-    const roles = user?.['https://your_domain/roles'] || [];
+    const roles = user?.[`https://your_domain/roles`] || [];
     setIsUserAdmin(roles.includes('admin'));
   }, [user]);
+
+  useEffect(() => {
+    const loggedInFlag = sessionStorage.getItem('loggedIn');
+    if (loggedInFlag) {
+      // Do nothing, user session continues
+    } else if (isAuthenticated) {
+      sessionStorage.setItem('loggedIn', 'true');
+    }
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [isAuthenticated]);
+
+  const handleBeforeUnload = (event) => {
+    if (!sessionStorage.getItem('refreshing')) {
+      sessionStorage.removeItem('loggedIn');
+      logout({ returnTo: window.location.origin });
+    }
+  };
+
+  useEffect(() => {
+    sessionStorage.setItem('refreshing', 'true');
+    window.addEventListener('unload', () => {
+      sessionStorage.removeItem('refreshing');
+    });
+  }, []);
 
   if (isLoading) {
     return (
@@ -31,7 +60,6 @@ const App = () => {
 
   return (
     <Router>
-      {!isAuthenticated && <NavigationBar />}
       {isAuthenticated ? (
         <AuthenticatedApp
           user={user}
@@ -43,10 +71,9 @@ const App = () => {
           setShowParticipantIDPopup={setShowParticipantIDPopup}
           isProfileModalVisible={isProfileModalVisible}
           setIsProfileModalVisible={setIsProfileModalVisible}
-          setParticipantID = {setParticipantID}
-          setChatbotLoaded = {setChatbotLoaded}
-          participantID = {participantID}
-
+          setParticipantID={setParticipantID}
+          setChatbotLoaded={setChatbotLoaded}
+          participantID={participantID}
         />
       ) : (
         <UnauthApp />
--- src/components/UnauthApp.js
+++ src/components/UnauthApp.js
@@ -5,14 +5,18 @@ import { Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage/LandingPage';
 import AboutPage from './AboutPage/AboutPage';
 import ProductPage from './ProductPage/ProductPage';
+import NavigationBar from './NavigationBar/NavigationBar';
 
 const UnauthApp = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/product" element={<ProductPage />} />
-    </Routes>
+    <>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/product" element={<ProductPage />} />
+      </Routes>
+    </>
   );
 };
 
