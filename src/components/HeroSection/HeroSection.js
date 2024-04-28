2c63874a38bb03454d020e5450b73dc5351fc6ca
Junyi Zhou <junyi.zhou@emory.edu>
2024-04-28 11:13:50 -0400

    Make the blinking cursor on the first line diappear, remove the sleep-deprived heading

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -6,11 +6,11 @@ import heroImage from './HeroSection_Front_Image.jpeg';
 const HeroSection = () => (
     <div className="hero-section">
       <div className="text-content">
-        <p className="project-type">Sleep Deprived CS 370 Students</p>
+        <p className="project-type">Emory University</p>
         <h1 className="project-heading-1">PEDIATRIC AIRWAY</h1>
         <h1 className="project-heading-2">MANAGEMENT ASSISTANT</h1>
-        <p className="project-description">Leading tool in evaluating AI'/s effectiveness in Pediatric Airway Management. 
-                                            Our team of unpaid coders transformed the Pediatric Airway training procedure 
+        <p className="project-description">Leading tool in evaluating AI's effectiveness in Pediatric Airway Management. 
+                                            Our team transformed the Pediatric Airway training procedure 
                                             into a Chatbot that assists you instead.
         </p>
       </div>
