45a0c8c951e3b4f6687000e32195cf0a82128936
zhaoliangchen <david.chen2@emory.edu>
2024-04-30 05:01:36 -0400

    completion api

---
+++ backend/CompletionsAPI/main.py
@@ -0,0 +1,59 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import json
+
+
+class BMVAssistant:
+    def __init__(self, json_file, instruction_text=None):
+        load_dotenv()
+        self.client = OpenAI()
+
+        # load instruction text
+        if instruction_text is not None:
+            self.instruction = instruction_text
+        else:
+            with open("backend/CompletionsAPI/instruction_text.txt", "r") as file:
+                self.instruction = file.read()
+
+        # load data file
+        if isinstance(json_file, str):
+            with open(json_file, 'r') as file:
+                self.data = json.load(file)
+        else:
+            self.data = json_file
+
+        # initialization
+        self.instruction_and_data = f"Instructions: \n {self.instruction} \n Case Description: \n{self.data}"
+        self.chat_history = [{"role": "system", "content": self.instruction_and_data}]
+        self.chat_history_without_system_message = []
+
+    def submit_message(self, user_message):
+        message = {"role": "user", "content": user_message}
+        self.chat_history.append(message)
+        self.chat_history.append({"role": "system", "content": self.instruction_and_data})
+        self.chat_history_without_system_message.append(message)
+
+    def generate_response(self, streaming):
+        return self.client.chat.completions.create(
+            model="gpt-4-turbo-preview",
+            messages=self.chat_history,
+            stream=streaming
+        )
+
+    def get_response(self, print_output=False):
+        response = self.generate_response(streaming=False)
+        output = response.choices[0].message.content
+        if print_output:
+            print(output)
+        self.chat_history.append({"role": "assistant", "content": output})
+        self.chat_history_without_system_message.append({"role": "assistant", "content": output})
+        return response.choices[0].message.content
+
+
+def initialize(case_description, instruction_text):
+    assistant = BMVAssistant(case_description, instruction_text)
+    return assistant
+
+
+def reset(case_description, instruction_text):
+    initialize(case_description, instruction_text)
--- backend/app.py
+++ backend/app.py
@@ -1,76 +1,57 @@
 import os
 from flask import Flask, Response, request, jsonify, send_from_directory
 from pymongo import MongoClient
-from flask_cors import CORS 
+from flask_cors import CORS
 from flask_cors import cross_origin
-from AssistantAPICall.main import *  # Make sure this import includes all needed functions
+# from AssistantAPICall.main import *  # Make sure this import includes all needed functions
+from CompletionsAPI.main import *
 from dotenv import load_dotenv
 from datetime import datetime, timezone
 from bson import json_util
 import base64
+
 load_dotenv()
 
-
-app = Flask(__name__) 
+app = Flask(__name__)
 CORS(app)
 
-# Set up MongoDB connection 
-client = MongoClient(os.environ.get('MONGO_DB_CONNECTION_STRING', 'default_connection_string'))
+# Set up MongoDB connection
+client = MongoClient(os.environ.get('MONGO_DB_CONNECTION_STRING', 'your_default_connection_string'))
 db = client[os.environ.get('DB_NAME', 'Chatbot_Data')]
 
 temp_message = ""
 
 # Assuming global variables for managing conversation state
-conversation_thread = None
-conversation_run = None
+bmv_assistant = None
+case_description = None
 global_participant_id = None
+with open("./CompletionsAPI/instruction_text.txt", "r") as file:
+    instruction_text = file.read()
 
-# @app.route('/api/instruction-text', methods=['GET'])
-# def get_instruction_text():
-#     try:
-#         collection = db['instruction']
-#         instruction_data = collection.find_one({})  # Assuming there's only one entry
-#         print(instruction_data['content'])
-#         if instruction_data:
-#             return jsonify(instruction_data['content'])  # Assuming the content is stored under the 'content' key
-#         else:
-#             return jsonify("No instruction found"), 404
-#     except Exception as e:
-#         return jsonify(str(e)), 500
-
-# @app.route('/api/save-instruction-text', methods=['POST'])
-# def save_instruction_text():
-#     collection = db['instruction']
-#     data = request.get_json()
-#     text = data['text']
-#     try:
-#         result = collection.update_one({}, {'$set': {'content': text}}, upsert=True)
-#         return jsonify("File saved successfully"), 200 if result.modified_count > 0 else jsonify("No changes made"), 200
-#     except Exception as e:
-#         return jsonify(str(e)), 500
 
 @app.route('/api/instruction-text', methods=['GET'])
 def get_instruction_text():
-    directory_path = './AssistantAPICall/'
+    directory_path = './CompletionsAPI/'
     filename = 'instruction_text.txt'
     return send_from_directory(directory_path, filename)
+
 
 @app.route('/api/save-instruction-text', methods=['POST'])
 def save_instruction_text():
     data = request.get_json()
     text = data['text']
-    with open('backend/AssistantAPICall/instruction_text.txt', 'w') as file:
+    with open('CompletionsAPI/instruction_text.txt', 'w') as file:
         file.write(text)
     return {'status': 'File saved successfully'}
+
 
 @app.route('/reset-conversation', methods=['POST'])
 def reset_conversation():
     print('resetting conversation')
-    global conversation_thread, conversation_run
-    conversation_thread = None
-    conversation_run = None
-    reset()
+    global bmv_assistant, case_description, instruction_text
+    bmv_assistant = reset(case_description, instruction_text)
     return '', 204  # No Content response
+
 
 @app.route('/submit-chat-history', methods=['POST'])
 @cross_origin()
@@ -81,8 +62,8 @@ def store_conversation():
     if not data:
         return jsonify({'error': 'No data provided'}), 400
     conversation_data = {
-        'participantID': global_participant_id, #Store participantID with each conversation
-        'timestamp': datetime.now(timezone.utc), # Store the current timestamp
+        'participantID': global_participant_id,  #Store participantID with each conversation
+        'timestamp': datetime.now(timezone.utc),  # Store the current timestamp
         'history': data
     }
     try:
@@ -91,6 +72,7 @@ def store_conversation():
         return {'status': 'History uploaded successfully'}
     except Exception as e:
         print(f"An error occurred while inserting to MongoDB: {e}")
+
 
 def get_conversation_history_from_db(participant_id):
     try:
@@ -103,6 +85,7 @@ def get_conversation_history_from_db(participant_id):
         print(f"An error occurred while fetching data: {e}")
         return []
 
+
 @app.route('/get-conversation-history/<participant_id>', methods=['GET'])
 def get_history(participant_id):
     if not participant_id:
@@ -113,6 +96,7 @@ def get_history(participant_id):
         return jsonify({'error': 'No conversation history found for this ID'}), 404
 
     return jsonify({'conversationHistory': history}), 200
+
 
 @app.route('/set-participant-id', methods=['POST'])
 def set_participant_id():
@@ -128,20 +112,16 @@ def set_participant_id():
 @app.route('/init-conversation', methods=['POST'])
 @cross_origin()
 def init_conversation():
-
     print("Received init-conversation request with data:", request.json)
-
-    global conversation_thread, conversation_run, simulationFile
-    simulationFile = next(db['case'].aggregate([{'$sample': {'size': 1}}]), None)
-    if conversation_thread is None or conversation_run is None:
-        # Only initialize if not already done
-        data = request.json
-        init_input = data.get('init_input', '')  # Optional initial input to start the conversation
-        conversation_thread, conversation_run, simulationFile = initialize(init_input, simulationFile)
-        print("Conversation intialized")
+    global bmv_assistant, case_description, instruction_text
+    case_description = next(db['case'].aggregate([{'$sample': {'size': 1}}]), None)
+    if bmv_assistant is None:
+        bmv_assistant = initialize(case_description, instruction_text)
+        print("Conversation initialized")
         return jsonify({"message": "Conversation initialized"}), 200
     else:
-      return jsonify({"error": "Invalid request", "details": str(request.json)}), 400
+        return jsonify({"error": "Invalid request", "details": str(request.json)}), 400
+
 
 @app.route('/submit-data', methods=['POST'])
 @cross_origin()
@@ -150,39 +130,37 @@ def submit_data():
     result = db.cases.insert_one(data)
     return jsonify({"message": "Data received", "id": str(result.inserted_id)}), 200
 
+
 @app.route('/submit-user-input', methods=['POST'])
 @cross_origin()
 def submit_user_input():
-    global conversation_thread, conversation_run, simulationFile, temp_message
-    if conversation_thread is None or conversation_run is None:
+    global bmv_assistant, temp_message
+    if bmv_assistant is None:
         return jsonify({"error": "Conversation not initialized"}), 400
 
     data = request.json
-    user_input = data.get('text')
-    #participantID = data.get('participantID')  # Retrieve participantID from request data
-
+    user_input = data['text']
     if not user_input:
         return jsonify({"error": "No input provided"}), 400
-    
-    # Submit the new user input to the ongoing conversation
-    conversation_run = submit_message(conversation_thread, user_input, simulationFile)
-    
-    # Wait for the run to complete and fetch the last message
-    wait_on_run(conversation_run, conversation_thread)
-    last_message = getChatHistory(conversation_thread)[-1]
+
+    bmv_assistant.submit_message(user_input)
+    last_message = bmv_assistant.get_response()
     temp_message = last_message
-    print(last_message)
-    
+    #print(last_message)
+    #print(bmv_assistant.chat_history)
+    #print(bmv_assistant.chat_history_without_system_message)
     return jsonify({"response": last_message})
+
 
 @app.route('/get-message', methods=['GET'])
 @cross_origin()
 def get_message():
     global temp_message
-    if conversation_thread is not None:
+    if bmv_assistant is not None:
         return jsonify({"message": temp_message})
     else:
         return jsonify({"error": "No conversation started"})
+
 
 @app.route('/chat-history/<participant_id>', methods=['GET'])
 def get_chat_history(participant_id):
@@ -234,10 +212,6 @@ def get_image(id):
             return jsonify({"error": "Image not found"}), 404
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-
-
-
 
 
 if __name__ == '__main__':
